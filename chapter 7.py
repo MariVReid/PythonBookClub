225c4e72da83d9eeb9c15c438759b34aa090889b
MariVReid <marivreid13@gmail.com>
2023-12-15 13:51:10 -0600

    Book club 12/15

--- chapter 7.py
+++ chapter 7.py
@@ -1,51 +1,81 @@
-### Bob's Problem to Fix
-### Dictionary in a dictionary...
-# dictionaries defined...              NOTE: Be careful of the commas!
-users = {
-	'aeinstein':
-		{
-		'first': 'albert',
-		'last': 'einstein',
-		'location': 'princeton',
-		},
-	'mcurie':
-		{
-		'first': 'marie',
-		'last': 'cury',
-		'location': 'paris',
-		},
-	'pwoods':
-		{
-		'first': 'phil',
-		'last': 'woods',
-		'location': 'UMN',
-		},
-} 	
+message = input("I am a parrot bawk\n") #input always stores response as a string
+print(message)
+snow_white = input("\nWho is the fairest of them all: ")
+print(f"{snow_white} is the fairest of them all")
 
-# print it all out...
-for username in users.items():               # the top level...
-	print(f"{username}")
+prompt = "\nWhat is your first name?"
+prompt += "\nOh and what is your last name? "
+name = input(prompt)
+print(f"Hi {name}")
 
-		
-print("\n-----\n")
+age = input("\nHow old are you? ")
+age=int(age)
+if age>= 18:
+    print('you are an adult!')
+else:
+    print('you are a child')
+    
+number= input("\nGive me a number ")
+number=int(number)
+if number %2==0: #The % sign gives the remainder of a division
+    print('the number is even\n')
+else:
+    print("the number is odd\n")
+    
 
+current_number=1
+while current_number <=5:
+	print(current_number)
+	current_number +=1
+ 
+prompt= "\nBawk I am a parrot bawk (Say STOP to shut me up) "
+message=""
+while message != "STOP":
+	message=input(prompt)
+	print(message)
+ 
+prompt= "\nBawk I am BETTER a parrot bawk (Say STOP to shut me up) "
+message=""
+active=True
+while active: #this is using a flag
+    message=input(prompt)
+    if message == 'STOP':
+        active = False
+    else:
+        print(message)
 
-for username, users in users.items():        # another level...
-	print(f"{users['last']}")
+prompt = "\nPlease enter the name of a state (Say STOP if done) "
+while True:
+    city= input(prompt)
+    if city== 'STOP':
+        break
+    else:
+        print(f"{city.title()} is a nice place to visit")
+        
+number = 0 
+while number<10:
+    number+=1
+    if current_number% 2==0:
+        continue
+    print(current_number)
+    
+# Start with users that need to be verified,
+# and an empty list to hold confirmed users.
+unconfirmed_users = ['alice', 'brian', 'candace']
+confirmed_users = []
+# Verify each user until there are no more unconfirmed users.
+# Move each verified user into the list of confirmed users.
+while unconfirmed_users:
+	current_user = unconfirmed_users.pop()
+	print(f"Verifying user: {current_user.title()}")
+	confirmed_users.append(current_user)
+#Display all confirmed users.
+print("\nThe following users have been confirmed:")
+for confirmed_user in confirmed_users:
+print(confirmed_user.title())
 
-
-test_name = "bob"
-print(f"\n----- {test_name.title()} -----\n")  # making sure this works...
-
-
-#for username, users in users.items():        # capitalize...  What!!!??  Why??
-#	print(f"{users['last'].title()}")
-#
-
-print("\n-----\n")
-
-
-for username, users in users.items():        # Another way... Argh! What's wrong here!
-	lastname = (users['last'])
-	firstname = (users['first'])
-	print(f"One of the scientists: {firstname.title()} {lastname.title()}")
+pets = ['dog', 'cat', 'dog', 'goldfish', 'cat', 'rabbit', 'cat']
+print(pets)
+while 'cat' in pets:
+	pets.remove('cat')
+print(pets)
